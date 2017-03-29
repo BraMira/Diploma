c3eb9eff61af75dde18e632dc85b2185ae81ef51
Mirjam Pergar <mirjam.pergar@gmail.com>
2017-03-29 11:42:03 +0200

    Popravljene napake iz sestanka

--- lema_31.m
+++ lema_31.m
@@ -2,20 +2,22 @@ function [b] = lema_31(b1,b2,A)
 %Input: enotska vektorja b1 in b2, za katera velja imag(bi'*A*bi)=0 za
 %i=1,2, za dano matriko A
 %Output: izotropni vektor b
+a1 = real(b1'*A*b1);
+a2 = real(b2'*A*b2);
 
-if real(b1'*A*b1)<0,
-    if real(b2'*A*b2)>0,
-        alfa1 = real(b1'*A*b1);
-        alfa2 = real(b2'*A*b2);
+if a1<0,
+    if a2>0,
+        alfa1 = a1;
+        alfa2 = a2;
     else
         disp('b1 in b2 imata enako (negativno) predznaèena realna dela')
         b=0;
         return
     end
-elseif real(b1'*A*b1)>0,
-    if real(b2'*A*b2)<0,
-        alfa2 = real(b1'*A*b1);
-        alfa1 = real(b2'*A*b2);
+elseif a1>0,
+    if a2<0,
+        alfa2 = a1;
+        alfa1 = a2;
     else
         disp('b1 in b2 imata enako (pozitivno) predznaèena realna dela, ne moremo ju uporabiti!')
         b=0;
