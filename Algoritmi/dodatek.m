88aa63b70286119496e2b24424562d5cea957ff0
Mirjam Pergar <mirjam.pergar@gmail.com>
2017-09-12 13:42:46 +0200

    Delete dodatek.m

--- Algoritmi/dodatek.m
+++
@@ -1,8 +0,0 @@
-
-Vbox = [x(:,1), xx(:,1), y(:,1),yy(:,1)];
-rvbox = [x(:,1)'*A*x(:,1), xx(:,1)'*A*xx(:,1), y(:,1)'*A*y(:,1), yy(:,1)'*A*yy(:,1)];
-tol = 1e-14;
-
-b = izboljsava(A,Vbox,rvbox,tol);
-napaka = abs(b'*A*b);
-korak = 4;
